ac605f163a2bd3ab0969e8cfa3701bc29e6aee09
Daniel Gomez Jaramillo <daniel.gomez@studentpartner.com>
2020-10-13 19:52:24 -0500

    Dashboard

---
+++ DotvvmStartup.cs
@@ -0,0 +1,46 @@
+﻿using DotVVM.Framework.Configuration;
+using DotVVM.Framework.ResourceManagement;
+using DotVVM.Framework.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserDashboard
+{
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
+    {
+        // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+
+            ConfigureRoutes(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+            ConfigureResources(config, applicationPath);
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+            config.RouteTable.Add("Default", "", "Views/Default.dothtml");
+            config.RouteTable.Add("Create", "Create", "Views/Operations/Create.dothtml");
+            config.RouteTable.Add("Detail", "Detail/{Id}", "Views/Operations/Detail.dothtml");
+            config.RouteTable.Add("Edit", "Edit/{Id}", "Views/Operations/Edit.dothtml");
+            config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));    
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+            // register code-only controls and markup controls
+        }
+
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
+        {
+            config.Resources.Register("Styles", new StylesheetResource()
+            {
+                Location = new UrlResourceLocation("~/assets/css/material-dashboard.css?v=2.1.2")
+            });
+        }
+
+		public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("temp");
+		}
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using DotVVM.Framework.Hosting;
+using DotVVM.Framework.Routing;
+using UserDashboard.DAL;
+using Microsoft.EntityFrameworkCore;
+using UserDashboard.BL.Services;
+
+namespace UserDashboard
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDataProtection();
+            services.AddAuthorization();
+            services.AddWebEncoders();
+            services.AddTransient(typeof(UserService));
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<DBContext>(options =>
+                {
+                    options.UseNpgsql("host=localhost;port=5432;Username=postgres;Password=1234;Database=dotvvm_db");
+                });
+            services.AddDotVVM<DotvvmStartup>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // use DotVVM
+            var dotvvmConfiguration = app.UseDotVVM<DotvvmStartup>(env.ContentRootPath);
+            dotvvmConfiguration.AssertConfigurationIsValid();
+            
+            // use static files
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(env.WebRootPath)
+            });
+        }
+    }
+}
